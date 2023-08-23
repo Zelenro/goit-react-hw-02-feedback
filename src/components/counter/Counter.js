04124c837af6356b23a568276a6bf259bc28ff0c
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-08-23 15:05:29 +0300

    firstTask

---
+++ src/components/counter/Counter.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+export class Counter extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClickGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleClickNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleClickBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button onClick={this.handleClickGood}>Good</button>
+        <button onClick={this.handleClickNeutral}>Neutral</button>
+        <button onClick={this.handleClickBad}>Bad</button>
+
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+}
