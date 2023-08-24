49cd564acf9d526e9d71eb7769013e7485b493d9
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-08-24 17:53:58 +0300

    delete h2 from Statistics

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -8,7 +8,6 @@ export const Statistics = ({ good, neutral, bad, total, positivePercentage }) =>
 
   return (
     <div>
-      <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
